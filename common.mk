1a44c03f7c2f45b15961645122cc9c47b9873825
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 -0500

    sdm660-common: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- common.mk
+++ common.mk
@@ -38,8 +38,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Audio
 PRODUCT_PACKAGES += \
