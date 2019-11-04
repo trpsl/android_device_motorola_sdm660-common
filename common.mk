f2b62e3bbf0c596d5ceeeb271e04db1bc23effc7
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2019-11-03 22:28:35 -0500

    sdm660-common: Commonize PRODUCT_ENFORCE_RRO_TARGETS
    * All SDM660 devices are using this flag
    
    Change-Id: I2daa383a8d8380a76c806bf4d95a65a78ac17fb1
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- common.mk
+++ common.mk
@@ -38,6 +38,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
